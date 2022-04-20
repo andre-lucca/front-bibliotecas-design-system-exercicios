2bfdaab226566769a959ae1c96f7afad1e3830f0
Daniel <danieldeandradelopes@gmail.com>
2022-04-20 09:57:46 -0300

    initial commit

---
+++ src/pages/AddProduct/index.js
@@ -0,0 +1,11 @@
+import './styles.css';
+
+function AddProduct() {
+  return (
+    <div className="container-add-product">
+      <h1>Add Product</h1>
+    </div>
+  );
+}
+
+export default AddProduct;
